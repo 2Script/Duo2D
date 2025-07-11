ad90a5cd956715a2a79956e51ba5f60ae8faef26
Artin Alavi <1361714+Arastais@users.noreply.github.com>
2025-07-11 13:46:03 -0600

    Change attribute class to use hybrid_ptr

--- include/Duo2D/vulkan/memory/attribute.hpp
+++ include/Duo2D/vulkan/memory/attribute.hpp
@@ -1,20 +1,21 @@
 #pragma once
 #include <concepts>
 #include <cstddef>
-#include <ostream>
 #include <type_traits>
-#include <variant>
+#include "Duo2D/core/hybrid_ptr.hpp"
+#include "Duo2D/traits/different_from.hpp"
+
 
 namespace d2d::vk {
     template<typename T> requires std::is_standard_layout_v<T>
     struct attribute {
-        constexpr attribute() noexcept = default;
+        inline attribute() noexcept : val(new T) {}
         template<typename U> requires (std::is_constructible_v<T, U> && !std::is_same_v<T*, std::remove_cvref_t<U>>)
-        constexpr attribute(U&& value) noexcept : local_value(std::forward<U>(value)), holds_ref(false) {}
-        constexpr attribute(T* const& value) noexcept : ref(value), holds_ref(true) {}
-
+        inline attribute(U&& value) noexcept : val(new T(std::forward<U>(value))) {}
+        constexpr attribute(T* value) noexcept : val(std::ref(*value)) {}
+        
     public:
-        template<typename U>
+        template<::d2d::impl::when_decayed_different_from<attribute> U>
         constexpr attribute& operator=(U&& value) noexcept {
             get_ref() = std::forward<U>(value);
             return *this;
@@ -25,13 +26,13 @@ namespace d2d::vk {
         constexpr T const* operator&()  const noexcept { return get_ptr(); }
         constexpr T*       operator->()       noexcept { return get_ptr(); }
         constexpr T*       operator&()        noexcept { return get_ptr(); }
-        constexpr T const* get_ptr() const noexcept { return holds_ref ? ref : &local_value; }
-        constexpr T*       get_ptr()       noexcept { return holds_ref ? ref : &local_value; }
+        constexpr T const* get_ptr() const noexcept { return val.get(); }
+        constexpr T*       get_ptr()       noexcept { return val.get(); }
         
-        constexpr operator const T&() const noexcept { return get_ref(); }
-        constexpr operator T&() noexcept { return get_ref(); }
-        constexpr const T& get_ref() const noexcept { return holds_ref ? *ref : local_value; }
-        constexpr T& get_ref() noexcept { return holds_ref ? *ref : local_value; }
+        constexpr operator T const&() const noexcept { return get_ref(); }
+        constexpr operator T      &()       noexcept { return get_ref(); }
+        constexpr T const& get_ref() const noexcept { return *val.get(); }
+        constexpr T      & get_ref()       noexcept { return *val.get(); }
 
     public:
         T& operator +=(const T& rhs) noexcept(noexcept(std::declval<T>()  += rhs)) requires (requires (T t){{t  += rhs} -> std::same_as<T&>;}) { return get_ref()  += rhs; }
@@ -44,17 +45,13 @@ namespace d2d::vk {
         T& operator>>=(const T& rhs) noexcept(noexcept(std::declval<T>() >>= rhs)) requires (requires (T t){{t >>= rhs} -> std::same_as<T&>;}) { return get_ref() >>= rhs; }
         T& operator<<=(const T& rhs) noexcept(noexcept(std::declval<T>() <<= rhs)) requires (requires (T t){{t <<= rhs} -> std::same_as<T&>;}) { return get_ref() <<= rhs; }
 
-        decltype(auto) operator[](std::size_t idx) noexcept(noexcept(std::declval<T>()[idx])) requires (requires (T t){t[idx];}) { return get_ref()[idx]; }
+        decltype(auto) operator[](std::size_t idx)       noexcept(noexcept(std::declval<T>()[idx]))       requires (requires (      T t){t[idx];}) { return get_ref()[idx]; }
         decltype(auto) operator[](std::size_t idx) const noexcept(noexcept(std::declval<const T>()[idx])) requires (requires (const T t){t[idx];}) { return get_ref()[idx]; }
 
     public:
         using value_type = T;
 
     private:
-        union { //TODO add constructor or force T to be trivially constructible
-            T local_value;
-            T* ref;
-        };
-        bool holds_ref = false;
+        hybrid_ptr<T> val;
     };
 }
